80dc3c6ecd8f9f26c22c49ea8a734886aa6e28f2
Adil Arain <addilarain00@gmail.com>
2025-07-21 18:21:36 +0500

    Time: 49 ms (62.80%) | Memory: 90.8 MB (61.31%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -3,8 +3,8 @@ public:
     bool containsDuplicate(vector<int>& nums) {
         unordered_set<int> seen;
 
-        for(int num : nums) {
-            if(seen.count(num)) {
+        for (int num : nums) {
+            if (seen.count(num)) {
                 return true;
             }
 
