4f5f7f6ccef23164b88f7ebaf99464520987b4ca
Adil Arain <addilarain00@gmail.com>
2025-07-27 20:49:46 +0500

    Time: 79 ms (69.36%) | Memory: 88.9 MB (49.29%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -7,15 +7,12 @@ public:
         for (int num : numSet) {
             if (numSet.find(num - 1) == numSet.end()) {
                 int length = 1;
-
                 while (numSet.find(num + length) != numSet.end()) {
                     length++;
                 }
-
                 longest = max(longest, length);
             }
         }
-
         return longest;
     }
 };
