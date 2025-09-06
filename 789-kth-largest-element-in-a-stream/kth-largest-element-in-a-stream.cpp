33b7e3636e7b3b16fb70abd85fa6e666d07dc1d8
Aadil Aarain <addilarain00@gmail.com>
2025-09-06 09:57:40 +0500

    Time: 0 ms (100.00%) | Memory: 33.2 MB (24.31%) - LeetSync

--- 789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.cpp
+++ 789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.cpp
@@ -1,30 +1,21 @@
 class KthLargest {
+    int num;
+    priority_queue<int, vector<int>, greater<int>> pq;
+
 public:
-    KthLargest(int k, vector<int>& nums) : k(k) {
-        for (int num : nums) {
-            if (minHeap.size() < k) {
-                minHeap.push(num);
-            } else if (num > minHeap.top()) {
-                minHeap.push(num);
-                if (minHeap.size() > k) {
-                    minHeap.pop();
-                }
-            }
+    KthLargest(int k, vector<int>& nums) {
+        num = k;
+        for (auto ele : nums) {
+            pq.push(ele);
+            if (pq.size() > k)
+                pq.pop();
         }
     }
 
     int add(int val) {
-        if (minHeap.size() < k) {
-            minHeap.push(val);
-        } else if (val > minHeap.top()) {
-            minHeap.push(val);
-            minHeap.pop();
-        }
-        return minHeap.top();
+        pq.push(val);
+        if (pq.size() > num)
+            pq.pop();
+        return pq.top();
     }
-
-private:
-    int k;
-    priority_queue<int, vector<int>, greater<int>>
-        minHeap; // Min-heap to store the k largest elements
 };
