9454a832e74387437c679a8cc5dcc216fc41f01a
Aadil Aarain <addilarain00@gmail.com>
2025-09-07 10:34:47 +0500

    Time: 2 ms (76.41%) | Memory: 38.4 MB (71.53%) - LeetSync

--- 621-task-scheduler/task-scheduler.cpp
+++ 621-task-scheduler/task-scheduler.cpp
@@ -1,40 +1,15 @@
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
-
-        if (n == 0)
-            return tasks.size();
-
-        priority_queue<pair<int, char>> pq;
-        unordered_map<int, int> freq;
-
-        for (int i = 0; i < tasks.size(); i++)
-            freq[tasks[i]]++;
-
-        for (auto x : freq)
-            pq.push({x.second, x.first});
-
-        int ans = 0;
-        while (pq.size() > 0) {
-            if (pq.top().first == 1) {
-                ans += pq.size();
-                break;
-            }
-            vector<pair<int, char>> v;
-            v.push_back(pq.top());
-            pq.pop();
-            ans += 1;
-            for (int i = 0; i < n; i++) {
-                if (pq.size() > 0) {
-                    v.push_back(pq.top());
-                    pq.pop();
-                }
-                ans++;
-            }
-            for (auto x : v)
-                if (x.first > 1)
-                    pq.push({x.first - 1, x.second});
+        vector<int> freq(26, 0);
+        for (char t : tasks) {
+            freq[t - 'A']++;
         }
-        return ans;
+
+        int maxFreq = *max_element(freq.begin(), freq.end());
+        int countMax = count(freq.begin(), freq.end(), maxFreq);
+
+        int part1 = (maxFreq - 1) * (n + 1) + countMax;
+        return max((int)tasks.size(), part1);
     }
 };
