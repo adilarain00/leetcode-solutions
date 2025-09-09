64e2da44a3f8e2080ac2a6d7c5adc96e0475ddba
Aadil Aarain <addilarain00@gmail.com>
2025-09-09 15:14:06 +0500

    Time: 0 ms (100.00%) | Memory: 14.1 MB (36.64%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.cpp
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -2,15 +2,19 @@ class Solution {
 private:
     void solve(int idx, int target, vector<int>& arr, vector<int>& list,
                vector<vector<int>>& result) {
+
         if (target == 0) {
             result.push_back(list);
             return;
         }
+
         for (int i = idx; i < arr.size(); ++i) {
             if (i > idx && arr[i] == arr[i - 1])
                 continue;
+
             if (arr[i] > target)
                 break;
+
             list.push_back(arr[i]);
             solve(i + 1, target - arr[i], arr, list, result);
             list.pop_back();
@@ -19,10 +23,12 @@ private:
 
 public:
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        sort(candidates.begin(), candidates.end());
         vector<vector<int>> result;
         vector<int> list;
+
+        sort(candidates.begin(), candidates.end());
         solve(0, target, candidates, list, result);
+
         return result;
     }
 };
