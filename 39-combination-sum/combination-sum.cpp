057a33144ce23aa84c8b7eb317c1973f9dbcdfe5
Aadil Aarain <addilarain00@gmail.com>
2025-09-10 23:37:23 +0500

    Time: 3 ms (55.15%) | Memory: 14.3 MB (45.62%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,29 +1,28 @@
 class Solution {
 public:
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<vector<int>> res;
-        vector<int> comb;
-        makeCombination(candidates, target, 0, comb, 0, res);
-        return res;
+    vector<vector<int>> res;
+
+    void dfs(vector<int>& candidates, int target, int idx, vector<int>& path) {
+        if (target == 0) {
+            res.push_back(path);
+            return;
+        }
+
+        if (target < 0)
+            return;
+
+        for (int i = idx; i < candidates.size(); i++) {
+            path.push_back(candidates[i]);
+
+            dfs(candidates, target - candidates[i], i, path);
+            path.pop_back();
+        }
     }
 
-private:
-    void makeCombination(std::vector<int>& candidates, int target, int idx,
-                         vector<int>& comb, int total,
-                         vector<vector<int>>& res) {
-        if (total == target) {
-            res.push_back(comb);
-            return;
-        }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<int> path;
 
-        if (total > target || idx >= candidates.size()) {
-            return;
-        }
-
-        comb.push_back(candidates[idx]);
-        makeCombination(candidates, target, idx, comb, total + candidates[idx],
-                        res);
-        comb.pop_back();
-        makeCombination(candidates, target, idx + 1, comb, total, res);
+        dfs(candidates, target, 0, path);
+        return res;
     }
 };
