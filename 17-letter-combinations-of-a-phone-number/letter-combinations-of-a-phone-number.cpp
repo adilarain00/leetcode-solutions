c5e8aebb8f233cb0c56a0512f30f24fee8215ebc
Aadil Aarain <addilarain00@gmail.com>
2025-09-11 11:47:02 +0500

    Time: 0 ms (100.00%) | Memory: 9.6 MB (14.57%) - LeetSync

---
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<string> res;
+    vector<string> mapping = {
+        "",    "",    "abc",  "def", "ghi",
+        "jkl", "mno", "pqrs", "tuv", "wxyz"
+    };
+
+    void backtrack(string& digits, int index, string current) {
+        if (index == digits.size()) {
+            res.push_back(current);
+            return;
+        }
+
+        string letters = mapping[digits[index] - '0'];
+        for (char ch : letters) {
+            backtrack(digits, index + 1, current + ch);
+        }
+    }
+
+    vector<string> letterCombinations(string digits) {
+        if (digits.empty())
+            return {};
+        backtrack(digits, 0, "");
+        return res;
+    }
+};
